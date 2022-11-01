9159b45959c99c54af0e928fc13a654b6a5863ec
Bellwen <1559364413@qq.com>
2022-11-01 22:26:20 +0800

    Site updated: 2022-11-01 22:26:14

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>// 存数据
+// 存数据
 // name：命名 data：数据
 function saveData(name, data) {
     localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
--- js/snowflake.js
+++ js/snowflake.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
     // 移动端不显示
   }
   else{
